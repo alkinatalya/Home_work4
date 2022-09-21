3077b827f6111ed3cd2ddf7e2fcd3e676bfe1ca7
Natalya <alkinatalya@yandex.com>
2022-09-21 11:31:00 +0400

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 1: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number1 = Prompt("Введите первое число:");
+int number2 = Prompt("Введите второе число:");
+ int result = 1;
+//Square(number);
+
+if (number2 > 0)
+{
+    if (number2 == 1)
+    {
+        System.Console.WriteLine($"Число {number1} в степени {number2} => {number1}");
+    }
+    else
+    {
+        for (int index = 1; index < number2+1; index++) 
+        {
+            result = result * number1;
+        }
+    System.Console.WriteLine($"Число {number1} в степени {number2} => {result}");
+    }
+
+}
+else
+{
+    System.Console.WriteLine("Второе число должно быть натуральным");
+}
